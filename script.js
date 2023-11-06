1b3920e09a2dad52d394ff37552fc545f0bc3639
Arfa Zia <arfazia2006@gmail.com>
2023-11-06 09:59:26 +0500

    Initialize game variables

--- script.js
+++ script.js
@@ -4,5 +4,19 @@ const dialog = document.querySelector('dialog');
 Form.addEventListener( 'submit' , () => { 
   const formData = new FormData(Form);
   const data = Object.fromEntries(formData);
-  console.log(data)
+  initializeGame(data)
 })
+
+const initializeVariables = (data) => {
+  data.choice = +data.choice;
+  data.board = [0,1,2,3,4,5,6,7,8];
+  data.player1 = 'X';
+  data.player2 = 'O';
+  data.round = 0;
+  data.currentPlayer = 'X';
+  data.gameOver  = false;
+}
+const initializeGame = (data) => {
+ initializeVariables(data);
+ console.log(data);
+}
